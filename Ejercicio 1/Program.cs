5cadaf4f2d20a3a8a21c3fdfc2cf3f192ea6b3cd
chrisbeltignino <assasin.tignino@gmail.com>
2023-03-01 08:21:48 -0300

    Actualizacion

--- Ejercicio 1/Program.cs
+++ Ejercicio 1/Program.cs
@@ -1,4 +1,18 @@
-﻿using System;
+﻿/*
+    Consigna
+    Realizar en C# una clase llamada Validador que posea un método estático llamado Validar con la siguiente firma:
+
+    bool Validar(int valor, int min, int max)
+
+    valor: dato a validar.
+    min: mínimo valor incluido.
+    max: máximo valor incluido.
+    Pedir al usuario que ingrese 10 números enteros. Validar con el método desarrollado anteriormente que estén dentro del rango -100 y 100.
+
+    Terminado el ingreso mostrar el valor mínimo ingresado, valor máximo ingresado y el promedio.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,33 +24,40 @@ namespace Ejercicio_1
     {
         static void Main(string[] args)
         {
-            Int32 numero;
-            float contador = 0;
-            Int32 minimo = 0;
-            Int32 maximo = 0;
-            Int32 acumulador = 0;
+            int numero;
+            int minimo = 0;
+            int maximo = 0;
+            int acumulador = 0;
             float promedio;
 
-            for (int i = 0; i < 5; i++)
+            for (int i = 1; i < 10; i++)
             {
-                Console.Write("Ingresar 5 numeros: ");
+                Console.Write($"Ingrese el número {i}: ");
                 numero = int.Parse(Console.ReadLine());
 
-                acumulador += numero;
-                contador++;
-
-                if (numero > maximo)
+                if (Validador.Validar(numero, -100, 100))
                 {
-                    maximo = numero;
+                    if (numero < minimo)
+                    {
+                        minimo = numero;
+                    }
+                    if (numero > maximo)
+                    {
+                        maximo = numero;
+                    }
+                    acumulador += numero;
                 }
-                if (numero < minimo)
+                else
                 {
-                    minimo = numero;
+                    Console.WriteLine("El número ingresado no está en el rango permitido (-100 a 100)");
+                    i--; // Restar uno a i para repetir el ciclo con la misma posición
                 }
             }
-            promedio = (float)(acumulador / contador);
+            promedio = (float)(acumulador / 10);
 
-            Console.WriteLine("Minimo: {0}, Maximo: {1}, Promedio: {2}", minimo, maximo, promedio);
+            Console.WriteLine($"El valor mínimo ingresado es: {minimo}");
+            Console.WriteLine($"El valor máximo ingresado es: {maximo}");
+            Console.WriteLine($"El promedio es: {promedio}");
             Console.ReadLine();
         }
     }
